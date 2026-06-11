568a001d487273cadd86f586259cbd08a103e7d5
Ojochegbe-hash <jeremiahmichaelojochegbe@gmail.com>
2026-06-11 17:22:19 +0100

    My intro.js first backend assignment

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Jeremiah Michael Ojochegbe";
+const age = 23;
+const favoriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite language:", favoriteLanguage);
